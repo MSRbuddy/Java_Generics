5d36412b8b2bd174fa66cab46fa23bfc0469e0a2
MeghanaDasari <meghanadasari222@gmail.com>
2023-07-06 22:42:59 +0530

    Java Generics : Find max problem RF-2 using GenericClass!

---
+++ Refactor2.java
@@ -0,0 +1,39 @@
+public class Refactor2
+{
+    public static void main(String[] args)
+    {
+        // initializing the values and finding the maximum
+        System.out.println("Maximum of Integers is : "+ MaximumGenericsRF2.toPrintMax(10,25,18));
+        System.out.println("Maximum of Float is : " + MaximumGenericsRF2.toPrintMax(10.7f,20.4f,37.7f));
+        System.out.println("Maximum of Strings is : " + MaximumGenericsRF2.toPrintMax("Mango","Banana","Apple"));
+    }
+}
+
+class MaximumGenericsRF2<T extends Comparable<T>>
+{
+    T x;
+    T y;
+    T z;
+    // Generate Constructors
+    public MaximumGenericsRF2(T x, T y, T z)
+    {
+        super();
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public static <T extends Comparable<T>> T toPrintMax(T x, T y, T z)
+    {
+        T max = x;
+        if (y.compareTo(max) > 0 && y.compareTo(z) > 0)
+        {
+            max = y;
+        }
+        if (z.compareTo(max) > 0)
+        {
+            max = z;
+        }
+        return max;
+    }
+}
